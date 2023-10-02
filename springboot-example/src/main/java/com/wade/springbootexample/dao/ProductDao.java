d1715f471b6c309e9d33cf7627d803b55d92ebfd
wade_home <pc121502806@gmail.com>
2023-10-02 16:47:12 +0800

    impl query productList API (base)

--- springboot-example/src/main/java/com/wade/springbootexample/dao/ProductDao.java
+++ springboot-example/src/main/java/com/wade/springbootexample/dao/ProductDao.java
@@ -3,8 +3,11 @@ package com.wade.springbootexample.dao;
 import com.wade.springbootexample.dto.ProductRequest;
 import com.wade.springbootexample.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- springboot-example/src/main/java/com/wade/springbootexample/service/ProductService.java
+++ springboot-example/src/main/java/com/wade/springbootexample/service/ProductService.java
@@ -3,7 +3,11 @@ package com.wade.springbootexample.service;
 import com.wade.springbootexample.dto.ProductRequest;
 import com.wade.springbootexample.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
 
