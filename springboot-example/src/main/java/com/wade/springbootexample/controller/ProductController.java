5bae66c5974a13faa2a52977472c660df0bbbc77
wade_home <pc121502806@gmail.com>
2023-10-03 15:30:46 +0800

    impl query productList API (pagination and total)

--- springboot-example/src/main/java/com/wade/springbootexample/controller/ProductController.java
+++ springboot-example/src/main/java/com/wade/springbootexample/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.wade.springbootexample.dto.ProductQueryParams;
 import com.wade.springbootexample.dto.ProductRequest;
 import com.wade.springbootexample.model.Product;
 import com.wade.springbootexample.service.ProductService;
+import com.wade.springbootexample.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +25,7 @@ public class ProductController {
     ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
             // Filtering
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String search,
@@ -43,9 +44,20 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        // get product list
         List<Product> productList = productService.getProducts(productQueryParams);
 
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        // get product total
+        Integer total = productService.countProduct(productQueryParams);
+
+        // 分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
     @GetMapping("/products/{productId}")
--- springboot-example/src/main/java/com/wade/springbootexample/dao/ProductDao.java
+++ springboot-example/src/main/java/com/wade/springbootexample/dao/ProductDao.java
@@ -8,12 +8,10 @@ import java.util.List;
 
 public interface ProductDao {
 
+    Integer countProduct(ProductQueryParams productQueryParams);
     List<Product> getProducts(ProductQueryParams productQueryParams);
     Product getProductById(Integer productId);
-
     Integer createProduct(ProductRequest productRequest);
-
     void updateProduct(Integer productId, ProductRequest productRequest);
-
     void deleteProductById(Integer productId);
 }
