53654740fd22a0e3567bbdc4ffd402a723706ad2
wade_home <pc121502806@gmail.com>
2023-10-04 15:37:14 +0800

    Encryption user password by MD5

--- springboot-example/src/main/java/com/wade/springbootexample/service/impl/UserServiceImpl.java
+++ springboot-example/src/main/java/com/wade/springbootexample/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -36,6 +37,10 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        // Generate password hash value by MD5
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         // Create user account
         return userDao.createUser(userRegisterRequest);
     }
@@ -44,12 +49,17 @@ public class UserServiceImpl implements UserService {
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
 
+        // Check user exists
         if (user == null) {
             log.warn("This email {} has not been registered yet", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        if (user.getPassword().equals(userLoginRequest.getPassword())) {
+        // Generate password hash value by MD5
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        // Compare password
+        if (user.getPassword().equals(hashedPassword)) {
             return user;
         } else {
             log.warn("This email {} password incorrect", userLoginRequest.getEmail());
